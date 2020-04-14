ac1820688d8af511b4e2af2dfdb6b097f8295746
m-maohuawei <mhw828@sina.com>
2020-04-14 10:43:17 +0800

    add StatusBarUtil

--- sdk/src/main/java/org/ms/module/base/view/BaseAppCompatActivity.java
+++ sdk/src/main/java/org/ms/module/base/view/BaseAppCompatActivity.java
@@ -63,7 +63,7 @@ public abstract class BaseAppCompatActivity<P extends IPresenter> extends AppCom
         }
 
         super.onCreate(savedInstanceState);
-
+        setStatusBar();
 
         if (getLayout() != 0) {
             setContentView(getLayout());
@@ -80,6 +80,12 @@ public abstract class BaseAppCompatActivity<P extends IPresenter> extends AppCom
     protected abstract int getLayout();
 
     protected abstract boolean isFullScreen();
+
+
+    protected void setStatusBar() {
+        //这里做了两件事情，1.使状态栏透明并使contentView填充到状态栏 2.预留出状态栏的位置，防止界面上的控件离顶部靠的太近。这样就可以实现开头说的第二种情况的沉浸式状态栏了
+        StatusBarUtil.setTransparent(this);
+    }
 
 
     public void showDialog() {
